dc01189d7f0dd4bfadb4696d657b075b0c7cc714
ross caubarreaux <ross.caubarreaux@sixactualsoftware.com>
2017-05-30 07:45:23 -0600

    Updated Read Me and added Exception Documentation in Word COntroller for Swagger.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# anagram
-Word/Anagram API
+# Anagram REST API
+
+Project for managing words within a mongoDB and fetching anagrams based on given input. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+* Java 8
+* Maven (3.3.9 used)
+* MongoDB (3.2.9 used)
+* Spring Boot 
+
+### Deploying locally
+
+Make sure MongoDB is running an run:
+
+```
+mvn spring-boot:run
+```
+
+## Running Tests
+
+```
+mvn test
+```
+
+## Swagger Documentation
+
+```
+http://localhost:8080/swagger-ui.html
+```
+
+## Built With
+
+* [Java 8](http://docs.oracle.com/javase/8/docs/)
+* [Spring Boot](https://projects.spring.io/spring-boot/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [MongoDB](https://docs.mongodb.com/?_ga=2.13110334.880675633.1496115831-269637611.1496115831) - Database 
+
+## Authors
+
+* **Ross Caubarreaux** [rcaubarreaux](https://github.com/rcaubarreaux)
--- src/main/java/com/caubarreaux/controller/WordController.java
+++ src/main/java/com/caubarreaux/controller/WordController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * User: ross
@@ -35,6 +37,11 @@ public class WordController {
         value = "Get all words",
         response = WordIO.class
     )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @RequestMapping(value = {"/words"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<WordIO> getAllWords() throws BaseApiException {
         return new ResponseEntity<WordIO>(wordService.getAllWords(), HttpStatus.OK);
@@ -45,6 +52,12 @@ public class WordController {
             value = "Add Word(s)",
             response = ResponseEntity.class
     )
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 400, message = "No input provided"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @RequestMapping(value = {"/words"}, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity insertWord(@RequestBody WordIO words) throws BaseApiException {
         wordService.insertAllWords(words.getWords());
@@ -56,6 +69,12 @@ public class WordController {
             value = "Delete a Word",
             response = ResponseEntity.class
     )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "No input provided"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @RequestMapping(value = "/words/{word}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity removeWord(@PathVariable String word) throws BaseApiException {
         wordService.deleteWord(word);
@@ -67,6 +86,12 @@ public class WordController {
             value = "Delete all Words",
             response = ResponseEntity.class
     )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 204, message = "No Content"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @RequestMapping(value = {"/words"}, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity removeWords() throws BaseApiException {
         wordService.deleteAllWords();
@@ -78,6 +103,12 @@ public class WordController {
             value = "Get Anagrams",
             response = Anagram.class
     )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "No input provided"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @RequestMapping(name = "anagrams", value = "/anagrams/{word}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Anagram> getAllAnagrams(@PathVariable("word") String word, @RequestParam(required = false) String limit) throws BaseApiException {
         Anagram results = wordService.getAnagramsOfWord(word, limit);
